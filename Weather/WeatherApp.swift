a1d7c624b172d49366faa8c031fd1b5e12845565
sashapivovarova <78265489+sashapivovarova@users.noreply.github.com>
2023-10-25 18:05:20 +0900

    Initial Commit

---
+++ Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Anastasia Grey on 2023/10/25.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
