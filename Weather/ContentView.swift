1cb0ac77c22517bfb21faafb3be13d1c1d348b2f
sashapivovarova <78265489+sashapivovarova@users.noreply.github.com>
2023-10-31 17:03:34 +0900

    trailing clousure の理解

--- Weather/ContentView.swift
+++ Weather/ContentView.swift
@@ -8,33 +8,21 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State var result: String = "...."
+    
     
     var body: some View {
         VStack {
-            Button {
-                result = sumTwoNumber(4, 2)
-                result = minusTwoNumber(4, 2)
-                result = calculateTwoNumber(4, 2, calculate: {first, second in return first + second})
-                result = calculateTwoNumber(4, 2, calculate: {first, second in return first - second})
-                result = calculateTwoNumber(4, 2, calculate: {first, second in return first * second})
-            }label: {
-                Text("Calculate")
-            }
-            Text(result)
+            Text("sasha")
+                .onAppear {
+                    trailingClosure(first: 3, second: 4) { a, b in
+                        print("\(a)+\(b)")
+                    }
+                }
         }
     }
     
-    func sumTwoNumber(_ first: Int, _ second: Int) -> String {
-        return String(first + second)
-    }
-    
-    func minusTwoNumber(_ first: Int, _ second: Int) -> String {
-        return String(first - second)
-    }
-    
-    func calculateTwoNumber(_ first: Int, _ second: Int, calculate: (Int, Int) -> Int) -> String {
-        return String(calculate(first, second))
+    func trailingClosure(first: Int, second: Int, action: (Int, Int)->()) {
+        action(first, second)
     }
 }
 
