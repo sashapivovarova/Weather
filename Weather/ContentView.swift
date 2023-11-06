9d1984b6f754dd7712293a2ff529847d6d91a7f6
sashapivovarova <78265489+sashapivovarova@users.noreply.github.com>
2023-11-06 22:38:35 +0900

    protocol の理解

--- Weather/ContentView.swift
+++ Weather/ContentView.swift
@@ -7,26 +7,47 @@
 
 import SwiftUI
 
+protocol Vehicle {
+    var name: String { get }
+    var speed: Int { get }
+}
+
 struct ContentView: View {
     
-    struct Pet {
+    struct Car: Vehicle {
         let name: String
-        let ownerName: String
-        let type: String
+        let speed: Int
     }
     
-    let myPet = Pet(name: "Cherry", ownerName: "Sasha", type: "Shiba")
-    let myPet2 = Pet(name: "Happy", ownerName: "Sasha", type: "Jindo")
+    struct Train: Vehicle {
+        let name: String
+        let speed: Int
+    }
+    
+    struct Airplane: Vehicle {
+        let name: String
+        let speed: Int
+    }
+    
+    //    let car = Car(name: "Power", speed: 3)
+    //    let train = Train(name: "PoPo", speed: 14)
+    
+    let vehicle: [Vehicle] = [Car(name: "Power", speed: 3), Train(name: "PoPo", speed: 14),Airplane(name: "Jet", speed: 800)]
     
     var body: some View {
         VStack{
-            Text(myPet.name)
-            Text(myPet.ownerName)
-            Text(myPet.type)
-            Divider()
-            Text(myPet2.name)
-            Text(myPet2.ownerName)
-            Text(myPet2.type)
+            HStack{
+                Text(vehicle[0].name)
+                Text(vehicle[0].speed.description)
+            }
+            HStack{
+                Text(vehicle[1].name)
+                Text(vehicle[1].speed.description)
+            }
+            HStack{
+                Text(vehicle[2].name)
+                Text(vehicle[2].speed.description)
+            }
         }
     }
 }
