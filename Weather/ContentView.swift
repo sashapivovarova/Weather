d9ced98d85283363372d476d2185ac090ac3a45f
sashapivovarova <78265489+sashapivovarova@users.noreply.github.com>
2023-11-06 22:45:24 +0900

    concurrency の理解1

--- Weather/ContentView.swift
+++ Weather/ContentView.swift
@@ -7,47 +7,23 @@
 
 import SwiftUI
 
-protocol Vehicle {
-    var name: String { get }
-    var speed: Int { get }
-}
 
 struct ContentView: View {
     
-    struct Car: Vehicle {
-        let name: String
-        let speed: Int
-    }
-    
-    struct Train: Vehicle {
-        let name: String
-        let speed: Int
-    }
-    
-    struct Airplane: Vehicle {
-        let name: String
-        let speed: Int
-    }
-    
-    //    let car = Car(name: "Power", speed: 3)
-    //    let train = Train(name: "PoPo", speed: 14)
-    
-    let vehicle: [Vehicle] = [Car(name: "Power", speed: 3), Train(name: "PoPo", speed: 14),Airplane(name: "Jet", speed: 800)]
+    @State var myName = "Sasha"
     
     var body: some View {
         VStack{
-            HStack{
-                Text(vehicle[0].name)
-                Text(vehicle[0].speed.description)
+            Text(myName)
+            Button("Delay!") {
+                downloadFile()
             }
-            HStack{
-                Text(vehicle[1].name)
-                Text(vehicle[1].speed.description)
-            }
-            HStack{
-                Text(vehicle[2].name)
-                Text(vehicle[2].speed.description)
-            }
+        }
+    }
+    
+    func downloadFile() {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+            myName = "Done"
         }
     }
 }
