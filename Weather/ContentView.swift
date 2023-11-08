bcd431c1cd4dc8f6596da2d7357e2e2e15b8501b
sashapivovarova <78265489+sashapivovarova@users.noreply.github.com>
2023-11-08 21:45:26 +0900

    Shape の理解

--- Weather/ContentView.swift
+++ Weather/ContentView.swift
@@ -10,21 +10,10 @@ import SwiftUI
 
 struct ContentView: View {
     
-    @State var myName = "Sasha"
-    
     var body: some View {
-        VStack{
-            Text(myName)
-            Button("Delay!") {
-                downloadFile()
-            }
-        }
-    }
-    
-    func downloadFile() {
-        DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
-            myName = "Done"
-        }
+        Image(systemName: "heart")
+            .resizable()
+            .clipShape(Rectangle())
     }
 }
 
