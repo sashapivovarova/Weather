8a85af2882cc7fc082eb2528d55ca3cad1531239
sashapivovarova <78265489+sashapivovarova@users.noreply.github.com>
2023-11-14 22:20:46 +0900

    colorhex の理解

--- Weather/ContentView.swift
+++ Weather/ContentView.swift
@@ -10,16 +10,25 @@ import SwiftUI
 
 struct ContentView: View {
     
-    @State var inputText: String = "Please intput the information about you."
-    
     var body: some View {
         VStack{
-            TextEditor(text: $inputText)
+            //#FFAAC1
+            //255 170 194
+            //Color(red: 255/255.0, green: 170/255.0, blue: 194/255.0)
+            Color(0xFFAAC1)
         }
-        .padding()
-        .background(.pink)
-        .frame(height: 300)
-        .multilineTextAlignment(.center)
+    }
+}
+
+extension Color {
+    init(_ hex: UInt, alpha: Double = 1) {
+        self.init(
+            .sRGB,
+            red: Double((hex >> 16) & 0xFF) / 255,
+            green: Double((hex >> 8) & 0xFF) / 255,
+            blue: Double((hex >> 0) & 0xFF) / 255,
+            opacity: alpha
+        )
     }
 }
 
