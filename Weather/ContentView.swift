edb7cd03f2bd6ffd0706fb263a4cbdac9e2583e5
sashapivovarova <78265489+sashapivovarova@users.noreply.github.com>
2023-11-16 21:58:18 +0900

    gradient の理解

--- Weather/ContentView.swift
+++ Weather/ContentView.swift
@@ -13,31 +13,15 @@ struct ContentView: View {
     @State var isLighting: Bool = false
     
     var body: some View {
-        VStack{
-            Image(systemName: "cloud")
-                .offset(y: -200)
-            
-            HStack{
-                Image(systemName: "bolt")
-                    .rotationEffect(isLighting ? .degrees(0) : .degrees(180+360))
-                    .offset(y: isLighting ? 0 : -200)
-                    .padding()
-                    .animation(.easeInOut(duration: 2), value:isLighting)
-                Image(systemName: "bolt")
-                    .offset(y: isLighting ? 0 : -200)
-                    .padding()
-                    .animation(.easeIn(duration: 2), value:isLighting)
-                Image(systemName: "bolt")
-                    .offset(y: isLighting ? 0 : -200)
-                    .padding()
-                    .animation(.easeOut(duration: 2), value:isLighting)
-            }
-            
-            Button {
-                isLighting.toggle()
-            } label: {
-                Text("Click")
-            }
+        ZStack{
+            LinearGradient(gradient: Gradient(colors: [Color.white, Color.peach]),
+                           startPoint: .topLeading,
+                           endPoint: .bottomTrailing)
+            .ignoresSafeArea()
+            Text("Sasha")
+                .background(LinearGradient(gradient: Gradient(colors: [Color.white, Color.peach]),
+                                           startPoint: .topLeading,
+                                           endPoint: .bottomTrailing))
         }
     }
 }
