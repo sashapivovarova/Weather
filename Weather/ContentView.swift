f4cd78ecc8c20dde368a492c9161eca7383c8bd4
sashapivovarova <78265489+sashapivovarova@users.noreply.github.com>
2023-11-20 21:46:50 +0900

    overlay の理解

--- Weather/ContentView.swift
+++ Weather/ContentView.swift
@@ -10,84 +10,39 @@ import SwiftUI
 
 struct ContentView: View {
     
-    @State var id: String = ""
-    @State var password: String = ""
-    @State var checking: Bool = false
-    @State var success: Bool = false
-    @State var fail: Bool = false
+    @State var width: CGFloat = 200
+    @State var height: CGFloat = 200
+    @State var color: Color = .peach
     
     var body: some View {
-        VStack {
-            HStack{
-                idLabel()
-                TextField("Enter ID", text: $id)
-                    .textFieldStyle(.roundedBorder)
-            }
-            HStack{
-                pwLabel()
-                if checking {
-                    TextField("Enter Password", text: $password)
-                        .textFieldStyle(.roundedBorder)
-                } else {
-                    SecureField("Enter Password", text: $password)
-                        .textFieldStyle(.roundedBorder)
-                }
-                Button {
-                    checking.toggle()
-                }label: {
-                    Image(systemName: "eye")
-                }
-            }
-            Button {
-                success = checkLoggedIn(id: id, pw: password)
-            } label: {
-                Text("Log in")
-            }
-            .fullScreenCover(isPresented: $success) {
-                ZStack{
-                    Color.orange.ignoresSafeArea()
-                    VStack{
-                        Text("Welcome, \(id)")
-                        Button {
-                            success = false
-                            fail = true
-                        }label: {
-                            Text("Log out")
+        ZStack {
+            //            Circle()
+            //                .foregroundColor(.peach)
+            //                .frame(width: 300, height: 300)
+            //            Circle()
+            //                .foregroundColor(.yellow)
+            //                .frame(width: 200, height: 200)
+            //            Circle()
+            //                .foregroundColor(.blue)
+            //                .frame(width: 100, height: 100)
+            
+            Circle()
+                .foregroundColor(.peach)
+                .frame(width: 300, height: 300)
+                .overlay {
+                    targetView(width: 200, height: 200, color: .yellow)
+                        .overlay {
+                            targetView(width: 100, height: 100, color: .blue)
                         }
-                    }
                 }
-            }
-            .alert(isPresented: $fail) {
-                Alert(title: Text("Password is wrong!"))
-            }
-            .padding()
         }
     }
     
     @ViewBuilder
-    func idLabel() -> some View {
-        Label {
-            Text("ID : ")
-        } icon: {
-            Image(systemName: "person.fill")
-        }
-    }
-    
-    @ViewBuilder
-    func pwLabel() -> some View {
-        Label {
-            Text("PW : ")
-        } icon: {
-            Image(systemName: "lock.fill")
-        }
-    }
-    
-    private func checkLoggedIn(id: String, pw: String) -> Bool {
-        if id == "sasha" && pw == "1234" {
-            return true
-        } else {
-            return false
-        }
+    func targetView(width: CGFloat, height: CGFloat, color: Color) -> some View {
+        Circle()
+            .foregroundColor(color)
+            .frame(width: width, height: height)
     }
 }
 
